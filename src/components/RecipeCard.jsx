1eb90aa8a2528c94c82a7ec5aabe993e783fef06
sharathchandra596 <sharathjoshi596@gmail.com>
2024-06-16 19:08:31 +0530

    api intigration completed and showing data on the screen

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,17 +1,17 @@
-import { Heart, HeartPulse, Soup } from 'lucide-react'
+import { Heart, HeartPulse, Soup, Youtube } from 'lucide-react'
 
 
-function RecipeCard() {
+function RecipeCard({recipe}) {
   return (
     <div className="flex flex-col rounded-md bg-[#c6cca1] overflow-hidden p-3 relative">
-            <a href="#" className=" relative h-32">
+            < a className=" relative h-32">
               <img
-                src="/public/1.jpg"
+                src={recipe.image}
                 alt="img"
                 className=" rounded-md w-full h-full object-cover cursor-pointer"
               />
-              <div className=" absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-[10px]">
-                <Soup size={"15"} /> 4 servings
+              <div className=" absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-[10px] font-semibold">
+                <Soup size={"15"} /> {Math.round(recipe.calories) +"cals"}
               </div>
               <div className=" absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
                 <Heart
@@ -21,18 +21,28 @@ function RecipeCard() {
               </div>
             </a>
               <div className="flex mt-1">
-                <p>rosted chiken</p>
+                <p>{recipe.label}</p>
               </div>
 
-              <p className="my-2">Turkesh Kitchine</p>
+              <div className="my-2 capitalize flex justify-between">
+                <span>{recipe.cuisineType}</span>
+
+                <div className='rounded-md  cursor-pointer bg-[#ebc687] p-1'>  
+                  <a href={`https://www.youtube.com/results?search_query=${recipe.label}`} target='_blank'>
+                  <Youtube size={30} className=''/> 
+                 
+                  </a>
+                  </div>
+
+              </div>
               <div className="flex gap-2 mt-auto">
                 <div className="flex gap-1 bg-[#f8ddb3] items-center p-1 rounded-md">
                   <HeartPulse size={16}/>
-                  <span className=" text-sm tracking-tighter font-semibold">gluten free</span>
+                  <span className=" text-sm tracking-tighter font-semibold">{recipe.healthLabels[1]}</span>
                 </div>
                 <div className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md">
                   <HeartPulse size={16}/>
-                  <span className=" text-sm tracking-tighter font-semibold">heart</span>
+                  <span className=" text-sm tracking-tighter font-semibold">{recipe.healthLabels[2]}</span>
                 </div>
                 
               </div>
--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -1,7 +1,40 @@
-import { Heart, HeartPulse, Search, Soup } from "lucide-react";
+
+import { Search } from "lucide-react";
 import RecipeCard from "../RecipeCard";
+import { useEffect, useState } from "react";
+const App_id="bae19a4b"
+const App_key="eb06050bb6e6a96cd56269c599b594c8"
 
 function Homepage() {
+
+  const [recipe, setRecipe]=useState([])
+
+  const [loading,setloading]=useState(false)
+  
+
+ async function fetchRecipes(searchQueary)
+  {
+    setloading(true)
+    setRecipe([])
+    try {
+      const res= await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${App_id}&app_key=${App_key}&q=chicken&type=public`)
+      const data= await res.json()
+      setRecipe(data.hits)
+      console.log(recipe);
+
+    } catch (error) {
+      console.log(error.message);
+    }
+    finally
+    {
+      setloading(false)
+    }
+  }
+
+  useEffect(()=>{
+    fetchRecipes("chicken")
+  },[])
+
   return (
     <div className=" bg-slate-100 p-5 flex-1">
       <div className=" max-w-screen-lg mx-auto">
@@ -24,12 +57,13 @@ function Homepage() {
 
         <div className=" grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {/* 1st recipe */}
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
+          {/* <RecipeCard/> */}
+
+          {loading&& <div>loading.......</div>}
+          {!loading&& recipe?.map(({recipe},index)=> <RecipeCard key={index} recipe={recipe}/>)}
+
+
+         
 
 
         </div>
